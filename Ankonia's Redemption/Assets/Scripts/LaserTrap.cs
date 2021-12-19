8fb2182493d62425724a9e8e1fcecced8cdac84c
yousef476 <yousefelking476@gmail.com>
2021-12-19 22:51:19 +0200

    lasers

---
+++ Ankonia's Redemption/Assets/Scripts/LaserTrap.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTrap : MonoBehaviour
+{
+    private float timeTilSpawn;
+    public float startTimeTilSpawn;
+
+    public GameObject laser;
+    public Transform whereToSpawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeTilSpawn <= 0)
+        {
+            Instantiate(laser, whereToSpawn.position, whereToSpawn.rotation);
+            timeTilSpawn = startTimeTilSpawn;
+        }
+        else
+        {
+            timeTilSpawn -= Time.deltaTime;
+        }
+    }
+}
