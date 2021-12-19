f166b0c2005ce34815625b53fcb98feb0b31de29
yousef476 <yousefelking476@gmail.com>
2021-12-20 01:24:01 +0200

    GAME OVER SCENE

---
+++ Ankonia's Redemption/Assets/Scripts/GameOverScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScene : MonoBehaviour
+{
+
+    public void Setup(){
+        gameObject.SetActive(true);
+    }
+
+    public void RestartButton(){
+        SceneManager.LoadScene("");
+        Debug.Log("Game Restarted");
+    }
+    public void ExitButton(){
+        SceneManager.LoadScene("StartMenu");
+    }
+}
