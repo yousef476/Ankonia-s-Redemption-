36838e6a8bca37c556b77da3591bf01d5f368c75
yousef476 <yousefelking476@gmail.com>
2021-12-20 16:48:13 +0200

    done

--- Ankonia's Redemption/Assets/Scripts/CheckPoints.cs
+++ Ankonia's Redemption/Assets/Scripts/CheckPoints.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserDestroy : MonoBehaviour
+public class CheckPoints : MonoBehaviour
 {
-    public float timeTilDestroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Destroy(gameObject, timeTilDestroy);
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
-            FindObjectOfType<levelManager>().RespawnPlayer();
+            FindObjectOfType<levelManager>().CurrentCheckpoint = this.gameObject;
     }
 }
